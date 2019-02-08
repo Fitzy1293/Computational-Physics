22843cd49b848c141df14d881b118a2fc7edf4ed
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2019-02-07 23:59:04 -0500

    Add files via upload

---
+++ dotProduct.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+#N-dimensional dot product, code is straight forward enough to read. 
+
+vectorU = []
+while True:
+    u = input('Enter vector u\'s component, or enter \'v\' to enter the next vector.\n')
+    if u.lower() == 'v':
+        print()
+        break
+    vectorU.append(int(u))
+
+vectorV = []
+while True:
+    v = input('Enter vector v\'s components, or enter \'Calculate\' to calculate the dot product.\n')
+    if v.lower() == 'calculate':
+        print()
+        break
+    vectorV.append(int(v))
+
+print()    
+
+print('Vector u: ' + str(vectorU))
+print('Vector v: ' + str(vectorV))
+
+npU = np.array(vectorU)
+npV = np.array(vectorV)
+
+if len(npU) != len(npV):
+    print('Vectors must have the same number of dimensions to calculate their dot product.')
+
+else:
+    dotProduct = 0
+    for i in range(len(npU)):
+        dotProduct = npU[i] * npV[i] + dotProduct
+
+    if dotProduct == 0:
+        print('These vectors are orthogonal, their dot product is 0.')
+    else:
+        print('These vectors are not orthogonal, their dotProduct is ' + str(dotProduct) + '.')
+        
+
