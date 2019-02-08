6da80e2da5e2623425057222740242a43717814b
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2019-02-08 03:00:10 -0500

    Update dotProduct.py

--- dotProduct.py
+++ dotProduct.py
@@ -3,19 +3,23 @@ import numpy as np
 #N-dimensional dot product, code is straight forward enough to read. 
 
 vectorU = []
+dimension = 1
 while True:
-    u = input('Enter vector u\'s component, or enter \'v\' to enter the next vector.\n')
-    if u.lower() == 'v':
+    u = input('Enter vector u\'s component #' + str(dimension) + ', or enter \'Next\' to enter the next vector.\n')
+    if u.lower() == 'next':
         print()
         break
+    dimension += 1
     vectorU.append(int(u))
 
 vectorV = []
+dimension = 1
 while True:
-    v = input('Enter vector v\'s component, or enter \'Calculate\' to calculate their dot product.\n')
+    v = input('Enter vector v\'s component #' + str(dimension) + ', or enter \'Calculate\' to calculate their dot product.\n')
     if v.lower() == 'calculate':
         print()
         break
+    dimension += 1
     vectorV.append(int(v))
 
 print()    
